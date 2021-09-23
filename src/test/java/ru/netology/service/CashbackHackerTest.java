a2e1ab92f62dfc028f8a21824c0089286a30d5b5
Victoria159 <v.s.bocha@gmail.com>
2021-09-23 14:47:45 +0300

    Added CashbackHackerTest.java

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackerTest {
+    @Test
+    public void shouldPurchaseFor2000(){
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        int amount = 2000;
+
+        int actual = cashbackHacker.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+@Test
+    public void  shouldPurchaseFor900(){
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        int amount = 900;
+
+        int actual = cashbackHacker.remain(amount);
+        int expected= 100;
+
+        assertEquals(actual, expected);
+}
+}
